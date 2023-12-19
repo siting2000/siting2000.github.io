cb378e97b0a4a561695bb57d0a1ac67ad523b4a0
siting2000 <chses994053310131@gmail.com>
2023-12-19 21:39:17 +0800

    20231219update
    20231219update

---
+++ script.js
@@ -0,0 +1,9 @@
+function abc(){
+var dt=Date();
+    document.writeln("Today is " + dt)
+}
+abc();
+//var str="hello";
+//alert("Hello\"Ting\"");
+//document.writeln(typeof(str));
+//console.log("error");
